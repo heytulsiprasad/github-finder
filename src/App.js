3a1febbc8ea8c51620efad3556723b7625c86aa5
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-01-20 17:44:33 +0530

    final build

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import About from "./components/pages/About";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
-    const [alert, setAlert] = useState(null);
-
     // async componentDidMount() {
     //   setLoading(true)
 
@@ -35,36 +34,29 @@ const App = () => {
     // clears users from state
 
     // Set ALert
-    const showAlert = (msg, type) => {
-        setAlert({ msg: msg, type: type });
-
-        setTimeout(() => setAlert(null), 2000);
-    };
 
     return (
         <GithubState>
-            <Router>
-                <div className="App">
-                    <Navbar title="GitHub Finder" icon="fab fa-github" />
-                    <div className="container">
-                        <Alert alert={alert} />
-                        <Switch>
-                            <Route
-                                exact
-                                path="/"
-                                render={props => (
-                                    <Fragment>
-                                        <Search setAlert={showAlert} />
-                                        <Users />
-                                    </Fragment>
-                                )}
-                            />
-                            <Route exact path="/about" component={About} />
-                            <Route exact path="/user/:login" component={User} />
-                        </Switch>
+            <AlertState>
+                <Router>
+                    <div className="App">
+                        <Navbar title="GitHub Finder" icon="fab fa-github" />
+                        <div className="container">
+                            <Alert />
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/about" component={About} />
+                                <Route
+                                    exact
+                                    path="/user/:login"
+                                    component={User}
+                                />
+                                <Route component={NotFound} />
+                            </Switch>
+                        </div>
                     </div>
-                </div>
-            </Router>
+                </Router>
+            </AlertState>
         </GithubState>
     );
 };
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const NotFound = () => {
+    return (
+        <Fragment>
+            <h1>Not Found</h1>
+            <p className="lead">The page you are looking for does not exist.</p>
+        </Fragment>
+    );
+};
+
+export default NotFound;
